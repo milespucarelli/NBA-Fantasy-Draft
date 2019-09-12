ab2517c40689a376537e012e11cd88d6ac413695
milespucarelli <miles.pucarelli@gmail.com>
2019-09-12 01:12:40 -0400

    Security vulnerability patch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authorized, only: [:new, :create]
+  skip_before_action :authorized, only: %i[new create]
 
   def new
     @user = User.new
